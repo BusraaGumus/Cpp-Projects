0709244e1ca5f50e0cfae684e3b0115f026fc314
Büşra Gümüş <238904105+BusraaGumus@users.noreply.github.com>
2026-08-10 23:22:11 +0300

    Hangman Game Studio

--- 03_hangman.cpp
+++ 03_hangman.cpp
@@ -9,9 +9,9 @@ using namespace std;
 int main(){
     srand(time(0));
     vector<string> ThemeNames = {"Fruits", "Vegetables", "Foods"};
-    vector<string> Fruits = {"Apple", "Banana", "Cherry", "Stawberry", "Grapes", "Orange", "Watermelon", "Pineapple", "Mango", "Peach"};
+    vector<string> Fruits = {"Apple", "Banana", "Cherry", "Strawberry", "Grapes", "Orange", "Watermelon", "Pineapple", "Mango", "Peach"};
     vector<string> Vegetables = {"Carrot", "Broccoli", "Cucumber", "Tomato", "Potato", "Lettuce", "Onion", "Eggplant", "Zucchini", "Spinach"};
-    vector<string> Foods = {"Pizza", "Burger", "Lazania", "Noodle", "Kebap", "Sushie", "Döner", "Lahmacun", "Pide", "Sarma"};
+    vector<string> Foods = {"Pizza", "Burger", "Lazania", "Noodle", "Kebap", "Sushie", "Doner", "Lahmacun", "Pide", "Sarma"};
 
     int RandomThemeIndex = rand() % + ThemeNames.size();
     string currentTheme = ThemeNames[RandomThemeIndex]; 
@@ -32,4 +32,75 @@ int main(){
         int randomIndex = rand() % + Foods.size();
         WordToGuess = Foods[randomIndex];
     }
+
+    string guessedWord(WordToGuess.length(), '_');
+    int chances = 6;
+    string guessedLetters = "";
+
+    cout << "=== HANGMAN GAME ===" << endl;
+    cout << "Theme: " << currentTheme << endl;
+
+    while( chances > 0 && guessedWord != WordToGuess)
+    {
+        // Mevcut durumu ekrana bas (Örn: _ a _ _ a)
+        cout << "\nWord:";
+        for( int i= 0; i< guessedWord.length(); i++)
+        {
+            cout << guessedWord[i] <<  " " ;
+        }
+        cout<<" \n ";
+
+        cout << "\nGuess Left: " << chances <<endl;
+        cout << "Used Letters: " << guessedLetters << endl;
+        cout << "Guess A Letter: ";
+
+        char guess;
+        cin >> guess;
+
+        if(guessedLetters.find(guess) != string::npos)
+        {
+            cout<<" U already tried this, try another letter."<< endl;
+            cout << "Theme: " << currentTheme << endl;
+            continue;
+        }
+
+        guessedLetters += guess;
+        guessedLetters += " ";
+
+        bool found = false;
+        for (int i =0; i < WordToGuess.length(); i++)
+        {
+            if (WordToGuess[i] == guess)
+            {
+                guessedWord[i] = guess; // Bildiği indeksteki tireyi harfe çevir
+                found = true;
+            }
+        }
+
+        if (found)
+        { 
+            cout<< "U got one, keep going."<<endl;
+            cout << "Theme: " << currentTheme << endl;
+        }
+        else{
+            cout << "=== HANGMAN GAME ===" << endl;
+            cout << "Theme: " << currentTheme << endl;
+            cout << "Wrong letter, try again !" << endl; 
+            chances--;
+        }
+    }
+    if (guessedWord == WordToGuess && chances >=0)
+    {
+    cout << "\nWord:";
+        for( int i= 0; i< guessedWord.length(); i++)
+        {
+            cout << guessedWord[i] <<  " ";
+        }
+        cout<<endl;
+        cout << "Congratulations, You Got it! " << endl;
+    }else{
+    cout<< "Game Over, U Lost Baby Girl." << endl;
+    cout<< "Word: "<< WordToGuess<<endl;
+    }
+    return 0;
 }
